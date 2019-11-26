54fdb413b06d8597a1e34fe28c103ae00fe486f5
Ogonnaya <hello@ogonnaya.com>
2019-11-25 20:23:18 -0600

    installed npm packages, updated bamazon database

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# bamazon
-Creating an amazon-like storefront with node &amp; mysql
+# Bamazon
+
+## Description
+
+This app is a command line based Amazon-like store front.  It will  will take in orders from customers and deplete stock from the store's inventory.
+
+## Getting Started
+
+### Instructions
+
+#### Customer View
+
+
+#### Manager View
+
+TBD
+
+## Built With
+
+- Javascript
+- Node.js
+- MySQL
+
+### NPM Packages
+
+- [MySQL](https://www.npmjs.com/package/mysql)
+- [Inquirer](https://www.npmjs.com/package/inquirer)
+
+## Author
+
+- **[Ogonnaya Oshagbemi](https://github.com/Ogonnaya)**
---
+++ bamazon.sql
@@ -0,0 +1,31 @@
+USE bamazon_DB;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("t-shirt", "clothing", 2, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("dress", "clothing", 1, 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("heels", "shoes", 1, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("tennis shoes", "shoes", 1, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("wallet", "handbags", 1, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("tote", "handbags", 1, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("earrings", "jewelry", 1, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("bracelet", "jewelry", 1, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("gloves", "accessories", 1, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("scarf", "accesories", 1, 200);
