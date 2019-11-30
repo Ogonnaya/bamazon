72a4d43e05bebcd3a5f713a46de8ab7ce0afe370
Ogonnaya <hello@ogonnaya.com>
2019-11-30 00:30:25 -0600

    manager can vew low inventory items

--- bamazonManager.js
+++ bamazonManager.js
@@ -21,6 +21,8 @@ connection.connect(function(err) {
 });
 
 var managerView = function() {
+  console.log("");
+
   inquirer
     .prompt({
       name: "getStarted",
@@ -49,13 +51,11 @@ var managerView = function() {
     });
 };
 
+// Function for manager to view product table information
 var viewProducts = function() {
   var query = "SELECT * FROM products";
   connection.query(query, function(err, res) {
     if (err) throw err;
-    console.log("");
-    console.log("Welcome to your Dashboard");
-    console.log("_____________________________\n");
 
     var showTable = new Table({
       head: ["ID", "Product", "Department", "Price", "Stock Quantity"],
@@ -76,8 +76,29 @@ var viewProducts = function() {
     managerView();
   });
 };
+
+// Function for manager to view items with low inventory - in this case, items with stock quantity below 100
 var lowInventory = function() {
-  console.log("This lets you see products that are low");
+  //   console.log("This lets you see products that are low");
+  var query = "SELECT * FROM products WHERE stock_quantity < 100";
+  connection.query(query, function(err, res) {
+    var showLowInventory = new Table({
+      head: ["ID", "Product", "Department", "Price", "Stock Quantity"],
+      colWidths: [10, 15, 15, 10, 10]
+    });
+    for (var i = 0; i < res.length; i++) {
+      showLowInventory.push([
+        res[i].id,
+        res[i].product_name,
+        res[i].department_name,
+        "$" + res[i].price,
+        res[i].stock_quantity
+      ]);
+    }
+    console.log(showLowInventory.toString());
+    console.log("");
+    managerView();
+  });
 };
 
 var addInventory = function() {
